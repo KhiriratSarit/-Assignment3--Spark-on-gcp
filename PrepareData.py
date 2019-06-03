e38c5f9b58330ec8afe2675b641f01e5721752bf
KhiriratSarit <43704003+KhiriratSarit@users.noreply.github.com>
2019-06-03 12:40:27 +0200

    Update the code for the matrix generation with size 1000 * 1000 which is saved in the txt file

---
+++ PrepareData.py
@@ -0,0 +1,10 @@
+# prepare the data set 
+import numpy as np 
+
+rows = 1000 
+cols = 2000
+A = np.random.randn(rows,cols)
+output_matrix = np.dot(np.transpose(A),A) 
+
+
+np.savetxt('testmatrix.txt', output_matrix, fmt='%d')
