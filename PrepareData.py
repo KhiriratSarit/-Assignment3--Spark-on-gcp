126694f6f63e5dd54b4220a0e73811c0579f42d3
KhiriratSarit <43704003+KhiriratSarit@users.noreply.github.com>
2019-06-04 09:25:26 +0200

    Update the small fractions of codes to run spark on two clusters on google cloud

--- PrepareData.py
+++ PrepareData.py
@@ -7,4 +7,4 @@ A = np.random.randn(rows,cols)
 output_matrix = np.dot(np.transpose(A),A) 
 
 
-np.savetxt('testmatrix.txt', output_matrix, fmt='%d')
+np.savetxt('testmatrix2500.txt', output_matrix, fmt='%d')
